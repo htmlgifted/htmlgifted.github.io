fd97e43d0f0286617c4dee4605374f5072f18056
Jeremy Henry <jeremy.henry2@gmail.com>
2014-08-09 22:56:04 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE php>
+<?php
+/* Don't remove this line. */
+require('http://blog.jeremyahenry.com/wp-blog-header.php');
+?>
+
+<?php get_header(); ?>
+
+	<div class="main">		
+
+		<div class="content">
+
+this is where my stuff goes...
+
+		</div>
+
+			<?php get_sidebar(); ?>
+		<div class="clearer"><span></span></div>
+
+	</div>
+<?php get_footer(); ?>
+
+</div>
+
